4d69ca6f056d980b4ede0a013ab4e1a6facc19bd
estie <estie@estienoMacBook-Pro-3.local>
2021-07-11 14:32:10 +0900

    Add: if

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
-    <p>
-      サンプル
+    <p v-if="msg.length > 0">
+      {{msg}}
     </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
